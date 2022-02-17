34f8fdd7c08c7447bc7aac8fb4fd090bfa5ffae5
ProfGarfield <>
2022-02-17 14:06:59 -0500

    removed testing code from targetSettings.lua

--- MechanicsFiles/targetSettings.lua
+++ MechanicsFiles/targetSettings.lua
@@ -152,16 +152,4 @@ end
 
 strat.registerMoveUnitsAfterTargetCreatedOrCapturedFn(moveUnitsAfterTargetCreatedOrCapturedFunction)
 
-function discreteEvents.onKeyPress(keyID)
-  for item in strat.iterateTargets() do
-    if strat.isTarget(item) == false then
-      error("target not recognized")
-    end
-  end
-  for key,item in pairs(object) do
-    if strat.isTarget(item) == true then
-      error(tostring(key).." recognized as a target")
-    end
-  end
-  print("checked stuff")
-end
+
